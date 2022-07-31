14e80c34b91c60daf924685711cad30ec3a3ca1a
Bruno Peres <bruno_16515@hotmail.com>
2022-07-31 20:24:45 -0300

    Aula 04 concluída

--- src/App.js
+++ src/App.js
@@ -22,8 +22,8 @@ function App() {
 
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/products/:id" element={<ProductDetails />} />
+          <Route path="/about/*" element={<About />} />
+          <Route path="/products/:id/*" element={<ProductDetails />} />
           <Route path="/products" element={<Products />} />
           <Route path="/redirect" element={<Navigate to="/about" />} />
           <Route
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,7 @@
-import { Route, useNavigate } from "react-router-dom"
+import { Route, useNavigate, Routes } from "react-router-dom";
 
 // nested routes
-import Offers from "./Offers"
+import Offers from "./Offers";
 
 export default function About() {
   const navigate = useNavigate();
@@ -9,14 +9,31 @@ export default function About() {
   return (
     <div className="content">
       <h2>About Us</h2>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime tempora nisi eligendi quod ut illo cumque quibusdam reprehenderit veniam. Cum eaque, nostrum fugit ut inventore assumenda perspiciatis sed maiores numquam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati nam repellat hic dolore officia sequi quis quod commodi iusto doloremque. Corporis in et placeat unde sapiente perspiciatis minus! Recusandae, ad!</p>
-      
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime tempora nisi eligendi quod ut illo cumque quibusdam reprehenderit veniam. Cum eaque, nostrum fugit ut inventore assumenda perspiciatis sed maiores numquam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati nam repellat hic dolore officia sequi quis quod commodi iusto doloremque. Corporis in et placeat unde sapiente perspiciatis minus! Recusandae, ad!</p>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime tempora
+        nisi eligendi quod ut illo cumque quibusdam reprehenderit veniam. Cum
+        eaque, nostrum fugit ut inventore assumenda perspiciatis sed maiores
+        numquam? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Obcaecati nam repellat hic dolore officia sequi quis quod commodi iusto
+        doloremque. Corporis in et placeat unde sapiente perspiciatis minus!
+        Recusandae, ad!
+      </p>
+
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime tempora
+        nisi eligendi quod ut illo cumque quibusdam reprehenderit veniam. Cum
+        eaque, nostrum fugit ut inventore assumenda perspiciatis sed maiores
+        numquam? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Obcaecati nam repellat hic dolore officia sequi quis quod commodi iusto
+        doloremque. Corporis in et placeat unde sapiente perspiciatis minus!
+        Recusandae, ad!
+      </p>
 
       <button onClick={() => navigate("/products")}>See our products</button>
-      {/* <Route path="/about/offers">
-        <Offers />
-      </Route> */}
+
+      <Routes>
+        <Route path="offers" element={<Offers />} />
+      </Routes>
     </div>
-  )
+  );
 }
