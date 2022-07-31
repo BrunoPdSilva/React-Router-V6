b8f08d4a84c52ef11b10ce68b75a0beaf5aaa5ff
Bruno Peres <bruno_16515@hotmail.com>
2022-07-31 20:13:25 -0300

    Aula 03 finalizada

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes, Navigate } from "react-router-dom";
+import { useState } from "react";
 
 // pages
 import Home from "./pages/Home";
@@ -7,6 +8,8 @@ import Products from "./pages/Products";
 import ProductDetails from "./pages/ProductDetails";
 
 function App() {
+  const [cartIsEmpty] = useState(true);
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -22,6 +25,13 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/products/:id" element={<ProductDetails />} />
           <Route path="/products" element={<Products />} />
+          <Route path="/redirect" element={<Navigate to="/about" />} />
+          <Route
+            path="/checkout"
+            element={
+              cartIsEmpty ? <Navigate to="/products" /> : <p>CHECKOUT</p>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </div>
